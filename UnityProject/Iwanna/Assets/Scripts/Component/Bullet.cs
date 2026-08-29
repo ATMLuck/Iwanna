61a9ababecaa3f635de236c2be633698dd6b8b30
ATMLuck <A.T.M.Luck@outlook.com>
2026-08-29 16:50:12 +0800

    feat:添加子弹贴图

--- UnityProject/Iwanna/Assets/Scripts/Component/Bullet.cs
+++ UnityProject/Iwanna/Assets/Scripts/Component/Bullet.cs
@@ -5,10 +5,14 @@ public class Bullet : MonoBehaviour
     public float speed = 15f;
     public float lifetime = 2f;
     private Rigidbody2D rb;
+    private Animator ani;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.flipX = rb.velocity.x < 0f;
         Destroy(gameObject, lifetime);
     }
 
@@ -17,7 +21,11 @@ public class Bullet : MonoBehaviour
         // 碰到 Ground 层的物体就销毁
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
-            Destroy(gameObject);
+            ani.SetBool("Bullet_is_Dead",true);
         }
+    }
+    public void Dis()
+    {
+        Destroy(gameObject);
     }
 }
